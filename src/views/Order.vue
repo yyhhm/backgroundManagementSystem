b9a2f006cbec09e8973c8bbb01e5001566e2aa47
yyhm <2269866015@qq.com>
2022-03-26 15:00:28 +0800

    cs

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -77,9 +77,8 @@
 			<el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange"
 			:current-page="currentPage" 
 			:page-sizes="[10,20,30,40]" :page-size="pageSize"
-				
-				 layout="total, sizes, prev, pager, next, jumper" :total="total">
-			</el-pagination>
+			layout="total, sizes, prev, pager, next, jumper" :total="total">
+			 </el-pagination>
 		</div>
 	</el-card>
 </template>
